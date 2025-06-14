2f9a25d04bb4ab7011b354abd967f865e46a6195
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-06-14 05:11:37 -0700

    change output format

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator,denominator):
     try:
         result = float(numerator)/float(denominator)
-        return f"The result of the the division is {result}"
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
